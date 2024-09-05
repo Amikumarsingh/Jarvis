86b5a781f18e2ae6cba6f28da3d95fbefa98b8c8
Amit kumar Singh <2004amitkumarsingh@gmail.com>
2024-09-05 17:14:38 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    " Mombattiye " : "https://www.youtube.com/watch?v=fYbCl5_8nDg&list=RDfYbCl5_8nDg&start_radio=1",
+    "SURMA KAALA" : "https://www.youtube.com/watch?v=bK3lslhN458&list=RDfYbCl5_8nDg&index=3",
+    "Mere Wall" : "https://www.youtube.com/watch?v=Hjcx6Emg6yI&list=RDfYbCl5_8nDg&index=14"
+}
